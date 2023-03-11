2dd696dea54f5ebc8dbd471cc3bc6071be283afd
Cauê Beloni <cbeloni@gmail.com>
2023-03-11 12:47:23 -0300

    Adicionado medições cetesb

---
+++ home/service/qualidade_ar.py
@@ -0,0 +1,7 @@
+class QualidadeAr:
+    def __init__(self, nome, situacao_rede, tipo_rede, data, qualidade):
+        self.nome = nome
+        self.situacao_rede = situacao_rede
+        self.tipo_rede = tipo_rede
+        self.data = data
+        self.qualidade = qualidade
--- home/views.py
+++ home/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .service.qualidade_ar import QualidadeAr
 
 # Create your views here.
 
 def index(request):
 
+    qualidade_santo_andre = QualidadeAr(nome="Santo André",
+                                        situacao_rede="A",
+                                        tipo_rede="A",
+                                        data="2023-03-11 10:00:00.000",
+                                        qualidade="N1 - BOA")
+    qualidade_capao = QualidadeAr(nome="Capão Redondo",
+                                        situacao_rede="A",
+                                        tipo_rede="A",
+                                        data="2023-03-11 10:00:00.000",
+                                        qualidade="N1 - BOA")
+    qualidade_carapicuiba = QualidadeAr(nome="Carapicuíba",
+                                        situacao_rede="A",
+                                        tipo_rede="A",
+                                        data="2023-03-11 10:00:00.000",
+                                        qualidade="N1 - BOA")
+    qualidade_ibirapuera = QualidadeAr(nome="Ibirapuera",
+                                        situacao_rede="A",
+                                        tipo_rede="A",
+                                        data="2023-03-11 12:00:00.000",
+                                        qualidade="Não coletado")
+
+    lista_qualidade_ar = []
+    lista_qualidade_ar.append(qualidade_santo_andre)
+    lista_qualidade_ar.append(qualidade_capao)
+    lista_qualidade_ar.append(qualidade_carapicuiba)
+    lista_qualidade_ar.append(qualidade_ibirapuera)
+
     # Page from the theme 
-    return render(request, 'pages/index.html')
+    return render(request, 'pages/index.html', { "lista_qualidade_ar": lista_qualidade_ar})
