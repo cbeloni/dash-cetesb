6a17ce622660412ec006004c3d44eb2afbb3db2b
Cauê Beloni <cbeloni@gmail.com>
2023-03-11 14:55:34 -0300

    refatorado

--- home/service/qualidade_ar.py
+++ home/service/qualidade_ar.py
@@ -5,3 +5,32 @@ class QualidadeAr:
         self.tipo_rede = tipo_rede
         self.data = data
         self.qualidade = qualidade
+
+def listar_todos():
+    qualidade_santo_andre = QualidadeAr(nome="Santo André",
+                                        situacao_rede="A",
+                                        tipo_rede="A",
+                                        data="2023-03-11 10:00:00.000",
+                                        qualidade="N1 - BOA")
+    qualidade_capao = QualidadeAr(nome="Capão Redondo",
+                                  situacao_rede="A",
+                                  tipo_rede="A",
+                                  data="2023-03-11 10:00:00.000",
+                                  qualidade="N1 - BOA")
+    qualidade_carapicuiba = QualidadeAr(nome="Carapicuíba",
+                                        situacao_rede="A",
+                                        tipo_rede="A",
+                                        data="2023-03-11 10:00:00.000",
+                                        qualidade="N1 - BOA")
+    qualidade_ibirapuera = QualidadeAr(nome="Ibirapuera",
+                                       situacao_rede="A",
+                                       tipo_rede="A",
+                                       data="2023-03-11 12:00:00.000",
+                                       qualidade="Não coletado")
+
+    lista_qualidade_ar = []
+    lista_qualidade_ar.append(qualidade_santo_andre)
+    lista_qualidade_ar.append(qualidade_capao)
+    lista_qualidade_ar.append(qualidade_carapicuiba)
+    lista_qualidade_ar.append(qualidade_ibirapuera)
+    return lista_qualidade_ar
