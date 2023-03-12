4199bc6575e3cde2c24449dec4e767544f44a990
Cauê Beloni <cbeloni@gmail.com>
2023-03-12 00:15:24 -0300

    Inclusão gráfico dinamico

--- home/service/qualidade_ar.py
+++ home/service/qualidade_ar.py
@@ -10,7 +10,8 @@ class QualidadeAr:
         self.data = data
         self.qualidade = qualidade
 class Detalhes:
-    def __init__(self, indice, data):
+    def __init__(self, nome, indice, data):
+        self.nome = nome
         self.indice = indice
         self.data = data
 
@@ -24,10 +25,6 @@ def listar_detalhes():
 @cached(cache=TTLCache(maxsize=1024, ttl=3600))
 def _get_cetesb():
     url = "https://arcgis.cetesb.sp.gov.br/server/rest/services/QUALAR/CETESB_QUALAR/MapServer/6/query?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry={%22xmin%22:-5244191.63658331,%22ymin%22:-2739503.0937498696,%22xmax%22:-5165920.11961926,%22ymax%22:-2661231.5767858177,%22spatialReference%22:{%22wkid%22:102100}}&geometryType=esriGeometryEnvelope&inSR=102100&outFields=*&outSR=102100"
-
-    response = requests.get(url)
-    url = "https://arcgis.cetesb.sp.gov.br/server/rest/services/QUALAR/CETESB_QUALAR/MapServer/6/query?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry={%22xmin%22:-5244191.63658331,%22ymin%22:-2739503.0937498696,%22xmax%22:-5165920.11961926,%22ymax%22:-2661231.5767858177,%22spatialReference%22:{%22wkid%22:102100}}&geometryType=esriGeometryEnvelope&inSR=102100&outFields=*&outSR=102100"
-
     response = requests.get(url)
     data = response.json()
     lista_qualidade_ar = []
@@ -43,20 +40,31 @@ def _get_cetesb():
     return lista_qualidade_ar
 
 def _listar_detalhes():
-    json_data = '{"M1": 2, "TM1": 1678550400000, "M2": 2, "TM2": 1678546800000, "M3": 2, "TM3": 1678543200000, "M4": 2, "TM4": 1678539600000, "M5": 2, "TM5": 1678536000000}'
+    url = "https://arcgis.cetesb.sp.gov.br/server/rest/services/QUALAR/CETESB_QUALAR/MapServer/0/query?f=json&where=1=1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*"
+    #json_data = '{"M1": 2, "TM1": 1678550400000, "M2": 2, "TM2": 1678546800000, "M3": 2, "TM3": 1678543200000, "M4": 2, "TM4": 1678539600000, "M5": 2, "TM5": 1678536000000}'
+    response = requests.get(url)
+    features = json.loads(response.content)['features']
 
-    data = json.loads(json_data)
-    keys = list(data.keys())
+    lista_completa_detalhes = []
+    for f in features:
+        print (f['attributes']['ID'])
+        print(f['attributes']['STATNM'])
+        nome = f['attributes']['STATNM']
+        del f['attributes']['ID']
+        del f['attributes']['STATNM']
+        detalhes = _atribute_detalhes(f['attributes'], nome)
+        lista_completa_detalhes.append(detalhes)
+    return lista_completa_detalhes
+
+def _atribute_detalhes(data, nome):
     lista_detalhes = []
-    for i in range(0, len(keys), 2):
+    keys = list(data.keys())
+    count = 24 if len(keys) > 24 else len(keys)
+    for i in range(0, count, 2):
         valueIndice = data[keys[i]]
-        valueData = datetime.fromtimestamp(data[keys[i+1]]/1000).strftime("%Y-%m-%d %Hh")
-        lista_detalhes.append(Detalhes(indice=valueIndice,data=valueData))
-    nome_detalhes = "São bernardo do Campo"
-    return lista_detalhes, nome_detalhes
+        valueData = datetime.fromtimestamp(data[keys[i + 1]] / 1000).strftime("%Y-%m-%d %Hh")
+        lista_detalhes.append(Detalhes(indice=valueIndice, data=valueData, nome=nome))
+    return lista_detalhes
 
 if __name__ == '__main__':
-
-    lista_detalhes, nome = _listar_detalhes()
-    print(lista_detalhes[0].data)
-    print(nome)
+    detalhes = listar_detalhes()
--- home/views.py
+++ home/views.py
@@ -5,8 +5,7 @@ from .service.qualidade_ar import listar_todos, listar_detalhes
 # Create your views here.
 
 def index(request):
-    detalhes, nome_detalhes = listar_detalhes()
+    detalhes = listar_detalhes()
     # Page from the theme 
     return render(request, 'pages/index.html', { "lista_qualidade_ar": listar_todos(),
-                                                 "detalhes": detalhes,
-                                                 "nome_detalhes":nome_detalhes})
+                                                 "detalhes": detalhes})
